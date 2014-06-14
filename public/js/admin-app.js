c1f7acce09d985716c96f53652320d6f5a10ecc2
Manoj Nama <manoj.nama@outlook.com>
2014-06-14 19:32:04 +0530

    Added partials and controllers for admin app

--- public/js/admin-app.js
+++ public/js/admin-app.js
@@ -2,6 +2,8 @@ var VideoAdminApp = angular.module("VideoAdminApp", ["ngRoute", "VideoApp.direct
 
 VideoAdminApp.config(["$routeProvider", function ($routeProvider) {
 	$routeProvider
-		.when("/", {template: "<div></div>", controller: AdminHomeController})
-		.otherwise({redirectTo: "/"});
+		.when("/media/:offset/:limit", {templateUrl: "/partials/admin/media.html", controller: MediaController})
+		.when("/users/:offset/:limit", {templateUrl: "/partials/admin/users.html", controller: UsersController})
+		.when("/settings", {templateUrl: "/partials/admin/settings.html", controller: SettingsController})
+		.otherwise({redirectTo: "/media/0/10"});
 }]);
--- public/js/admin-controllers.js
+++ public/js/admin-controllers.js
@@ -2,10 +2,23 @@ function MasterController ($scope, $location, $rootScope) {
 	console.log("MasterController");
 };
 
-function AdminHomeController ($scope) {
-	console.log("AdminHomeController");	
+function MediaController ($scope, $location, $rootScope, $http, $routeParams) {
+	console.log("MediaController");	
+	$rootScope.currentPage = "media";
+};
+
+function UsersController ($scope, $location, $rootScope, $http, $routeParams) {
+	console.log("MediaController");	
+	$rootScope.currentPage = "users";
+};
+
+function SettingsController ($scope, $location, $rootScope, $http, $routeParams) {
+	console.log("MediaController");	
+	$rootScope.currentPage = "settings";
 };
 
 
 MasterController.$inject = ["$scope", "$location", "$rootScope"];
-AdminHomeController.$inject = ["$scope"];
+MediaController.$inject = ["$scope", "$location", "$rootScope", "$http", "$routeParams"];
+UsersController.$inject = ["$scope", "$location", "$rootScope", "$http", "$routeParams"];
+SettingsController.$inject = ["$scope", "$location", "$rootScope", "$http", "$routeParams"];
