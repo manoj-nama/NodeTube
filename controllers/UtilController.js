f1f234326ebdf68d903cd0a68cdfd78aed02d33b
manoj nama <manoj.nama@outlook.com>
2014-07-05 20:02:40 +0530

    Added mediaFile path utility method

--- controllers/UtilController.js
+++ controllers/UtilController.js
@@ -22,3 +22,16 @@ var deleteFolderRecursive = function(path) {
     }
 };
 exports.deleteFolder = deleteFolderRecursive;
+
+exports.getMediaPathByMediaId = function (mediaId) {
+    var dirPath = _appBaseDir + _config.conversion.mediaPath + mediaId;
+    var filePath = dirPath + "/" + mediaId;
+    return {
+        directory: dirPath,
+        cover: filePath + "_cover.jpg",
+        original: filePath,
+        mp4: filePath + "_mp4",
+        ogg: filePath + "_ogg",
+        webm: filePath + "_webm"
+    }
+};
